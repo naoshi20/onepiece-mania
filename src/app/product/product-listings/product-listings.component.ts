8c289c5a836484d09e3419821770516135cd168c
Naoshi Tomisaka <naoshi@NaoshinoMacBook-puro.local>
2022-08-08 02:05:51 +0900

    fix products

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from 'src/app/products';
 
 @Component({
   selector: 'app-product-listings',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  products: any = [1, 2, 3, 4]
+  products: any = products;
   constructor() { }
 
   ngOnInit(): void {
